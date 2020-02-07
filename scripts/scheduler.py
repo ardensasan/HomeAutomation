9a3a26384285cc02b8bc9da2648c2735b1652d66
ardensasan <52578552+ardensasan@users.noreply.github.com>
2020-02-07 14:16:28 +0800

    - updated scheduler script

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -4,29 +4,38 @@ import datetime
 import time
 import RPi.GPIO as GPIO
 import mysql.connector
-connection = mysql.connector.connect(
-  host="localhost",
-  user="abaynfriends",
-  passwd="abaynfriends",
-  database="homeautomation"
-)
 while True:
+    connection = mysql.connector.connect(
+    host="localhost",
+    user="abaynfriends",
+    passwd="abaynfriends",
+    database="homeautomation"
+    )
     dateToday = datetime.datetime.now().strftime('%Y-%m-%d')
     timeToday = datetime.datetime.now().strftime('%H:%M')
     result = connection.cursor()
     result.execute("SELECT tbl_appliances.applianceID,tbl_appliances.applianceName,tbl_schedules.scheduleDate,TIME_FORMAT(tbl_schedules.scheduleTime, '%H:%i') AS `Time`, tbl_appliances.applianceOutputPin,tbl_schedules.scheduleAction,tbl_schedules.scheduleRepeat,tbl_schedules.scheduleID FROM tbl_schedules LEFT JOIN tbl_appliances ON tbl_schedules.scheduleApplianceID=tbl_appliances.applianceID")
     data = result.fetchall()
+    result.close()
     for row in data:
-        applianceID = row[0];
-        applianceName = row[1];
-        scheduleDate = str(row[2]);
-        scheduleTime = str(row[3]);
-        applianceOutputPin = row[4];
-        scheduleAction = row[5];
-        scheduleRepeat = row[6];
-        scheduleID = row[7];
-        if scheduleRepeat == None:
-            print("ww");
+        applianceID = None;
+        applianceName = None;
+        scheduleDate = None;
+        scheduleTime = None;
+        applianceOutputPin = None;
+        scheduleAction = None;
+        scheduleRepeat = None;
+        scheduleID = None;
+        applianceID = row[0]
+        applianceName = row[1]
+        scheduleDate = str(row[2])
+        scheduleTime = str(row[3])
+        applianceOutputPin = row[4]
+        scheduleAction = row[5]
+        scheduleRepeat = row[6]
+        scheduleID = row[7]
+        if scheduleRepeat == '':
+            print(scheduleDate,dateToday,scheduleTime,timeToday)
             if scheduleDate == dateToday and scheduleTime == timeToday:
                 if scheduleAction == 0:
                     cursor = connection.cursor()
@@ -39,7 +48,7 @@ while True:
                     cursor.close()
                     cursor = connection.cursor()
                     cursor.execute("INSERT INTO tbl_logs(logDateTime,logAppliance,logAction,logVia,logUser) VALUES (%s,%s,%s,%s,%s)",(dateToday+" "+timeToday,applianceName,0,2,1))
-                    connection.commit()	
+                    connection.commit() 
                     cursor.close()
                     subprocess.call(['python3', '/var/www/html/scripts/turnOFF.py', str(applianceOutputPin),])
                 if scheduleAction == 1:
@@ -84,13 +93,4 @@ while True:
                             connection.commit()
                             cursor.close()
                             subprocess.call(['python3', '/var/www/html/scripts/turnON.py', str(applianceOutputPin),])
-
-    result.close()
-time.sleep(10)
-
-
-
-
-
-
-
+time.sleep(50)
